9eec78fc3e13a7ef74a91c5960685cfa8814a544
iowaclassbattleshit <marc@schlaepfer.org>
2019-11-12 20:35:44 +0100

    feat(): sass loader works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Player } from "./classes/player.class";
+import "./styles/styles.scss"
 
 const i = new Player;
 i.do();
--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s[c]ss$/i,
         use: [
+          // Creates `style` nodes from JS strings
           'style-loader',
-          'sass-loader'
-        ]
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-
     ]
   },
   plugins: [
